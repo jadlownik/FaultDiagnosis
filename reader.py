80371b858923ce7759bf441d966cfdc509e7d0b5
jadlownik <karoluniekkukla@gmail.com>
2024-07-12 18:24:54 +0200

    results generated

--- reader.py
+++ reader.py
@@ -1,14 +1,16 @@
 from config import KNOWN_VARIABLES, FAULTS, UNKNOWN_VARIABLES, \
                     EQUATIONS, OBSERVATIONS, PATH_EXAMPLES
 import os
+import re
 
 
-class ExampleReader():
+class ExampleReader:
     _variables = None
 
-    def get_all_examples(folder_path):
+    def get_all_examples(self):
         examples = [f for f in os.listdir(PATH_EXAMPLES) if f.endswith('.txt')]
-        return examples
+        sorted_examples = sorted(examples, key=self._extract_number)
+        return sorted_examples
 
     def read(self, path):
         self._variables = {}
@@ -23,3 +25,7 @@ class ExampleReader():
                UNKNOWN_VARIABLES in self._variables and \
                EQUATIONS in self._variables and \
                OBSERVATIONS in self._variables
+
+    def _extract_number(self, filename):
+        match = re.search(r'(\d+)', filename)
+        return int(match.group(0)) if match else 0
